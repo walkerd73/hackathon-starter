4404e8292548a406d671ed512d3dcb854e6ceab9
david walker <walkerd73@gmail.com>
2020-03-30 11:38:48 -0500

    updated pem name

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '3.83.184.95',
       username: 'ubuntu',
-      privateKey: 'hackathon_starter-key.pem'
+      privateKey: 'hackathon-starter-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
